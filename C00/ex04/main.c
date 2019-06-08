c74ee36f3b1ebc8cecfde97f04c5ca610adf50d3
Bastien ADAM <badam@e2r12p16.42.fr>
2019-06-08 20:59:46 +0200

    Add newline

--- C00/ex04/main.c
+++ C00/ex04/main.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 20:19:12 by badam             #+#    #+#             */
-/*   Updated: 2019/06/08 20:51:45 by badam            ###   ########.fr       */
+/*   Updated: 2019/06/08 20:59:32 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,21 @@ int		main(void)
 {
 	write(1, "P:", 2);
 	ft_is_negative(1);
+	write(1, "\n", 1);
 	write(1, "P:", 2);
 	ft_is_negative(0);
-	write(1, "P:", 1);
+	write(1, "\n", 1);
+	write(1, "P:", 2);
 	ft_is_negative(2147483647);
+	write(1, "\n", 1);
 
 	write(1, "N:", 2);
 	ft_is_negative(-1);
+	write(1, "\n", 1);
 	write(1, "N:", 2);
 	ft_is_negative(-15);
+	write(1, "\n", 1);
 	write(1, "N:", 2);
 	ft_is_negative(-2147483648);
+	write(1, "\n", 1);
 }
