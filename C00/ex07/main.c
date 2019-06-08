c88fd78294332a14cfcd238369fa32ea256a5ace
Bastien ADAM <badam@e2r12p16.42.fr>
2019-06-08 21:01:17 +0200

    Add newline

--- C00/ex07/main.c
+++ C00/ex07/main.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 20:19:12 by badam             #+#    #+#             */
-/*   Updated: 2019/06/08 20:52:56 by badam            ###   ########.fr       */
+/*   Updated: 2019/06/08 21:01:06 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,24 @@ int		main(void)
 {
 	write(1, "42:", 3);
 	ft_putnbr(1);
+	write(1, "\n", 1);
 	write(1, "10:", 3);
 	ft_putnbr(10);
+	write(1, "\n", 1);
 	write(1, "0:", 2);
 	ft_putnbr(0);
+	write(1, "\n", 1);
 
 	write(1, "-42:", 4);
 	ft_putnbr(-42);
+	write(1, "\n", 1);
 	write(1, "-1:", 3);
 	ft_putnbr(-1);
+	write(1, "\n", 1);
 	write(1, "max:", 4);
 	ft_putnbr(2147483647);
+	write(1, "\n", 1);
 	write(1, "min:", 4);
 	ft_putnbr(-2147483648);
+	write(1, "\n", 1);
 }
