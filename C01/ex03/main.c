58ac578af9ce301c6ded7bcac4bda8482a58284e
Bastien ADAM <badam@e2r12p16.42.fr>
2019-06-09 15:35:22 +0200

    Patch

--- C01/ex03/main.c
+++ C01/ex03/main.c
@@ -6,13 +6,13 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 20:15:08 by badam             #+#    #+#             */
-/*   Updated: 2019/06/09 15:18:12 by badam            ###   ########.fr       */
+/*   Updated: 2019/06/09 15:35:07 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_div_mod(int *a, int *b, int *div, int *mod);
+void	ft_div_mod(int a, int b, int *div, int *mod);
 
 int		main(void)
 {
@@ -23,7 +23,7 @@ int		main(void)
 
 	a = 16;
 	b = 5;
-	ft_div_mod(&a, &b, &div, &mod);
+	ft_div_mod(a, b, &div, &mod);
 	if (div == 5 && mod == 1)
 	{
 		write(1, "OK\n", 3);
