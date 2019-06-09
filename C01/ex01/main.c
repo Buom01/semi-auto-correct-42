dea40557ed328abe9451359b099ccf71a7c7c965
Bastien ADAM <badam@e2r12p16.42.fr>
2019-06-09 15:33:41 +0200

    Patch

--- C01/ex01/main.c
+++ C01/ex01/main.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 20:15:08 by badam             #+#    #+#             */
-/*   Updated: 2019/06/09 15:18:44 by badam            ###   ########.fr       */
+/*   Updated: 2019/06/09 15:31:17 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_ultimate_ft(int *********nbr);
 int		main(void)
 {
 	int a = 3;
-	ft_ultimate_ft(&&&&&&&&&a);
+	ft_ultimate_ft(&a);
 	if (a == 42)
 	{
 		write(1, "OK\n", 3);
