823a3f5f6957cabe53b73eccd7854e2853829f01
Bastien ADAM <badam@e2r12p16.42.fr>
2019-06-10 22:08:59 +0200

    Patch

--- C02/ex09/main.c
+++ C02/ex09/main.c
@@ -6,13 +6,13 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 14:59:19 by badam             #+#    #+#             */
-/*   Updated: 2019/06/10 21:22:31 by badam            ###   ########.fr       */
+/*   Updated: 2019/06/10 22:05:50 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char	*ft_strlowcase(char *src);
+char	*ft_strcapitalize(char *src);
 
 void	ft_putstr(char *str)
 {
@@ -30,7 +30,7 @@ int		main()
 {
 	char c[] = "salut, coMMent tu vAs ? 42mOts quArAnTe-dEux; cinQuante+et+un";
 	ft_putstr("Salut, Comment Tu Vas ? 42mots Quarante-Deux; Cinquante+Et+Un:");
-	ft_putstr(c);
+	ft_putstr(ft_strcapitalize(c));
 	ft_putstr(":");
-	ft_putstr(ft_strlowcase(c));
+	ft_putstr(c);
 }
