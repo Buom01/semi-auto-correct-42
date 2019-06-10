f545280bac86b46d2e0a23c11cb50fefcb6a87ed
Bastien ADAM <badam@e2r12p16.42.fr>
2019-06-10 21:23:07 +0200

    Exo 08 et 09

---
+++ C02/ex08/main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 14:59:19 by badam             #+#    #+#             */
+/*   Updated: 2019/06/10 21:18:51 by badam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strlowcase(char *src);
+
+void	ft_putstr(char *str)
+{
+	int l;
+
+	l = 0;
+	while (str[l])
+	{
+		l++;
+	}
+	write(1, str, l);
+}
+
+int		main()
+{
+	char c[] = "AbCdEfG-Yz";
+	ft_putstr("abcdefg-yz:");
+	ft_putstr(c);
+	ft_putstr(":");
+	ft_putstr(ft_strlowcase(c));
+}
