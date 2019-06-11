86d7793e846e9058f3e2072947ba8c1549daaef7
Bastien ADAM <badam@e2r12p16.42.fr>
2019-06-11 16:14:51 +0200

    Finish C02

---
+++ C02/ex10/main.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/11 14:14:49 by badam             #+#    #+#             */
+/*   Updated: 2019/06/11 15:50:05 by badam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size);
+
+int	main(void)
+{
+	char src1a[] = "abcde";
+	char dest1a[] = "1234567";
+	char src1b[] = "abcde";
+	char dest1b[] = "1234567";
+	char src2a[] = "abcd";
+	char dest2a[] = "123";
+	char src2b[] = "abcd";
+	char dest2b[] = "123";
+	char src3a[] = "";
+	char dest3a[] = "";
+	char src3b[] = "";
+	char dest3b[] = "";
+	
+	printf("%s\n%s\n%s\n%s\n%s\n%s\n",
+			ft_strlcpy(dest1a, src1a, 5) == ft_strlcpy(dest1b, src1b, 5) ? "Pass" : "Fail",
+			strcmp(dest1a, dest1b) == 0 && strcmp(src1a, src1b) == 0 ? "Pass" : "Fail",
+			ft_strlcpy(dest2a, src2a, 3) == ft_strlcpy(dest2b, src2b, 3) ? "Pass" : "Fail",
+			strcmp(dest2a, dest2b) == 0 && strcmp(src2a, src2b) == 0 ? "Pass" : "Fail",
+			ft_strlcpy(dest3a, src3a, 3) == ft_strlcpy(dest3b, src3b, 2) ? "Pass" : "Fail",
+			strcmp(dest3a, dest3b) == 0 && strcmp(src3a, src3b) == 0 ? "Pass" : "Fail");
+}
---
+++ C02/ex11/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 14:59:19 by badam             #+#    #+#             */
+/*   Updated: 2019/06/11 16:04:08 by badam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_putstr_non_printable(char *src);
+
+void	ft_putstr(char *str)
+{
+	int l;
+
+	l = 0;
+	while (str[l])
+	{
+		l++;
+	}
+	write(1, str, l);
+}
+
+int		main()
+{
+	char c[] = "Coucou\ntu vas bien ?";
+	ft_putstr("Coucou\\0atu vas bien ?:");
+	ft_putstr_non_printable(c);
+}
