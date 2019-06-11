cc877793f3b63009b9d95177443908fa1c202de3
Bastien ADAM <badam@e2r12p16.42.fr>
2019-06-11 16:21:32 +0200

    Finish C02

--- C02/ex12/main.c
+++ C02/ex12/main.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 14:43:25 by badam             #+#    #+#             */
-/*   Updated: 2019/06/11 10:28:33 by badam            ###   ########.fr       */
+/*   Updated: 2019/06/11 16:20:43 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@ void	*ft_print_memory(void *addr, unsigned int size);
 int		main(void)
 {
 	char str[] = "Ceci est un test d'affichage de print memory!\nSegFault";
-	ft_print_memory(str, 56);
+	ft_print_memory(str, 54);
 }
