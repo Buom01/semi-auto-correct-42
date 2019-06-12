21c9432f455a5c8b8e561322bb387767d4f8a128
Bastien ADAM <badam@e2r12p16.42.fr>
2019-06-12 19:26:07 +0200

    Enchance tests

--- C02/ex10/main.c
+++ C02/ex10/main.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 14:14:49 by badam             #+#    #+#             */
-/*   Updated: 2019/06/11 21:15:39 by badam            ###   ########.fr       */
+/*   Updated: 2019/06/12 19:21:27 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,18 @@ int	main(void)
 	char dest3a[] = "";
 	char src3b[] = "";
 	char dest3b[] = "";
+	char src4a[] = "12345";
+	char dest4a[] = "abc";
+	char src4b[] = "12345";
+	char dest4b[] = "abc";
 	
-	printf("%s\n%s\n%s\n%s\n%s\n%s\n",
-			ft_strlcpy(dest1a, src1a, 5) == ft_strlcpy(dest1b, src1b, 5) ? "Pass" : "Fail",
+	printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
+			ft_strlcpy(dest1a, src1a, 5) == strlcpy(dest1b, src1b, 5) ? "Pass" : "Fail",
 			strcmp(dest1a, dest1b) == 0 && strcmp(src1a, src1b) == 0 ? "Pass" : "Fail",
-			ft_strlcpy(dest2a, src2a, 3) == ft_strlcpy(dest2b, src2b, 3) ? "Pass" : "Fail",
+			ft_strlcpy(dest2a, src2a, 3) == strlcpy(dest2b, src2b, 3) ? "Pass" : "Fail",
 			strcmp(dest2a, dest2b) == 0 && strcmp(src2a, src2b) == 0 ? "Pass" : "Fail",
-			ft_strlcpy(dest3a, src3a, 3) == ft_strlcpy(dest3b, src3b, 2) ? "Pass" : "Fail",
-			strcmp(dest3a, dest3b) == 0 && strcmp(src3a, src3b) == 0 ? "Pass" : "Fail");
+			ft_strlcpy(dest3a, src3a, 0) == strlcpy(dest3b, src3b, 0) ? "Pass" : "Fail",
+			strcmp(dest3a, dest3b) == 0 && strcmp(src3a, src3b) == 0 ? "Pass" : "Fail",
+			ft_strlcpy(dest4a, src4a, 2) == strlcpy(dest4b, src4b, 2) ? "Pass" : "Fail",
+			strcmp(dest4a, dest4b) == 0 && strcmp(src4a, src4b) == 0 ? "Pass" : "Fail");
 }
