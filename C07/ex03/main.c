4b9e756f747a36cc502061a8c7ee245eae097437
Bastien ADAM <badam@e2r12p16.42.fr>
2019-06-20 06:17:02 +0200

    Got exact same line on testing

--- C07/ex03/main.c
+++ C07/ex03/main.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 02:30:20 by badam             #+#    #+#             */
-/*   Updated: 2019/06/20 03:25:27 by badam            ###   ########.fr       */
+/*   Updated: 2019/06/20 06:16:40 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ char *ft_strjoin(int size, char **strs, char *sep);
 
 int	main(int argc, char **argv)
 {
-	printf("./output___said___this___is___a___success\n");
+	printf("./ex03/output___said___this___is___a___success :\n");
 	printf("%s\n",  ft_strjoin(argc, argv, "___"));
 }
